ff893870bf375ad6942cd8d60f578364e90edd38
Otto from Mac <operdeck@gmail.com>
2015-09-13 22:50:23 +0200

    Best XGB iteration: 50 Best XGB score: 0.757669 Number of vars:  2080 AUC val: 0.7576689 AUC dev: 0.8029059 Scoring test set. [1] "Written submission" total time: 17.8102 minutes

--- springleaf.R
+++ springleaf.R
@@ -1,12 +1,12 @@
-# This is the script that produces LB 0.7985 for me with more features engineering
+# "This is the script that produces LB 0.7985 for me with more features engineering"
 # Version 1 from https://www.kaggle.com/mrooijer/springleaf-marketing-response/xgboost-run-local/code
 
 # TODO, bring in features of predict.R:
-# - practicalities: use 'fread', rewrite wrinting of results
+# X practicalities: use 'fread', rewrite wrinting of results
 # - more rounds
-# - adding of date variants
-# - adding of 'countNA' feature
-# - handling of symbolics with -1 fields
+# X adding of date variants
+# X adding of 'countNA' feature
+# X handling of symbolics with -1 fields
 # - symbolic binning for 'character' columns
 # - numeric binning
 # - univariate selection
@@ -19,6 +19,8 @@ library(readr)
 library(data.table)
 library(bit64)
 library(lubridate)
+library(pROC)
+library(caret)
 
 #--------- P A R A M S ------------------------------------------------
 
@@ -41,7 +43,9 @@ version="local"
 set.seed(1948)
 epoch <- now()
 
-# -------- d a t a ---------------
+###########################
+# Data read
+###########################
 
 train <- fread( "data/train-2.csv",header = T, sep = ",",
                 stringsAsFactors=F,integer64="double",data.table=F )
@@ -52,7 +56,92 @@ test <- fread( "data/test-2.csv",header = T, sep = ",",
 testIDs <- test$ID
 test <- test[,-1]
 
+###########################
+# Data preparation
+###########################
+
+# Quickly replace missings by NA in symbolic fields
+# (should influence NA row count below)
+# BUT POTENTIALLY HARMFUL
+symNAs <- c("-1","")
+for (colName in colnames(train)[which(sapply(train, function(col) { return (!is.numeric(col)) } ))]) {
+  #   print(createSymbin(train[[colName]],train$target))
+  train[[colName]][train[[colName]] %in% symNAs] <- NA
+  test[[colName]][test[[colName]] %in% symNAs] <- NA
+  #   print(createSymbin(train[[colName]],train$target))
+}
+
+# Special extra fields - before doing any further processing
+# Add variable with nr of missing values per row. Make double to ensure numeric treatment.
+print("Counting NA's per row - time consuming")
+train$xtraNumNAs <- as.double(apply(train, 1, function(z) sum(is.na(z))))
+test$xtraNumNAs <- as.double(apply(test, 1, function(z) sum(is.na(z))))
+
+# Date field detection
+
+isDate <- function(vec) { 
+  all( grepl( "^\\d{2}[A-Z]{3}\\d{2}", vec[nzchar(vec)]) ) # check date fmt "12OCT13" (or empty)
+}
+
+dateFldNames <- colnames(train)[sapply(colnames(train), function(colName) 
+  { class(train[[colName]]) == "character" & isDate(na.omit(train[[colName]])) } )]
+cat("Date fields: ", dateFldNames, " (", length(dateFldNames), ")", fill=T)
+
+# Convert dates to time to epoch and add derived field(s) like weekday
+processDateFlds <- function(ds, colNames) {
+  result <- ds
+  for (colName in colNames) {
+    #cat("Date: ", colName, fill=T)
+    # what happens with NAs?
+    asDate <- strptime(ds[[colName]], format="%d%b%y")
+    result[[paste(colName, "_wday", sep="")]] <- wday(asDate)
+    result[[paste(colName, "_week", sep="")]] <- week(asDate)
+    result[[colName]] <- as.double(difftime(epoch, asDate,units='days'))
+  }
+  return(result)
+}
+
+# Create combinations of all possible date pairs. Perhaps some date differences are a good predictor.
+combineDates <- function(ds, fldNames) {
+  if (length(fldNames) >= 2) {
+    first <- fldNames[1]
+    rest <- fldNames[2:length(fldNames)]
+    
+    # add new column side by side with old one (new name)
+    for (second in rest) {
+      combinedName <- paste(first, second, sep="_")
+      #cat("Combine dates: ", combinedName, fill=T)
+      ds[[combinedName]] <- ds[[second]] - ds[[first]]
+    }
+    return(combineDates(ds, rest))
+  } else {
+    return(ds)
+  }
+}
+
+train <- processDateFlds(train, dateFldNames)
+test <- processDateFlds(test, dateFldNames)
+
+if (length(dateFldNames) > 0) {
+  train <- combineDates(train, dateFldNames)
+  test <- combineDates(test, dateFldNames)
+}
+
+###########################
 # Data analysis
+###########################
+
+print("Check (near) zero variance")
+
+# time consuming:
+# nfs <- nearZeroVar(train, saveMetrics = FALSE)
+
+# TODO switch back to nearZeroVar()
+zeroVarCols <- colnames(train)[sapply(colnames(train), function(colName) 
+  {return (length(unique(train[[colName]])) < 2)})]
+cat("Removed zero variance cols:", length(zeroVarCols), fill=T)
+train <- train[,!(names(train) %in% zeroVarCols)]
+test  <- test[,!(names(test) %in% zeroVarCols)]
 
 for (i in 1:ncol(train)) {
   if (class(train[[i]]) == "character") {
@@ -62,20 +151,26 @@ for (i in 1:ncol(train)) {
   }
 }
 
+###########################
 # NA handling
+###########################
 
 train[is.na(train)] <- -9999
 test[is.na(test)] <- -9999
 
 # some simple feature cleaning/engineering boosts the LB-AUC by 0.0025. 
 
+###########################
+# Model building
+###########################
+
 hold <- sample(1:nrow(train), 15000) #10% training data for stopping
 xgtrain = xgb.DMatrix(as.matrix(train[-hold,]), label = y[-hold], missing = NA)
 xgval = xgb.DMatrix(as.matrix(train[hold,]), label = y[hold], missing = NA)
 gc()
-watchlist <- list('val' = xgval)
+watchlist <- list('val' = xgval, 'dev' = xgtrain)
 model = xgb.train(
-  nrounds = 250   # increase for more results at home
+  nrounds = 2000   # increase for more results at home
   , params = param0
   , data = xgtrain
   , early.stop.round = 5
@@ -83,28 +178,32 @@ model = xgb.train(
   , print.every.n = 5
 )
 
-cat("Best XGB iteration:",model$bestInd,fill=T)
-cat("Best XGB score:",model$bestScore,fill=T)
+bst <- model$bestInd
+
+cat("Best XGB iteration:", bst, fill=T)
+cat("Best XGB score:", model$bestScore,fill=T)
 cat("Number of vars: ", length(colnames(train)), fill=T)
+
+cat("AUC val:", auc(y[hold], predict(model, xgval, ntreelimit=bst)), fill=T)
+cat("AUC dev:", auc(y[-hold], predict(model, xgtrain, ntreelimit=bst)), fill=T)
 
 # feature importance
 importance_mx <- xgb.importance(names(train), model=model)
 print( xgb.plot.importance(head(importance_mx,50) )) 
 
-# Watchlist best Score 0.775643
-
-# -------- w r i t e   r e s u l t  ------
+###########################
+# Score test set and write out
+###########################
 
 if (settings.doScoring) {
   
   cat("Scoring test set.",fill=T)
   rm("train")
-  rm("xgval"); gc()
-  train_val <- predict(model, newdata=xgtrain)
+  rm("xgval") 
   rm("xgtrain")
+  gc()
   xgtest <- xgb.DMatrix(as.matrix(test), missing = NA)
   
-  bst <- model$bestInd
   preds_out <- predict(model, xgtest, ntreelimit = bst)
 
   subm <- data.frame(testIDs, preds_out)
